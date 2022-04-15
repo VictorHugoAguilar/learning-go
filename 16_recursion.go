75c92cbadaba3c2759d70cbedcf9699fe7280cd1
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-04-15 09:59:46 +0200

    Add files via upload

---
+++ 16_recursion.go
@@ -0,0 +1,20 @@
+// Go soporta
+// <a href="http://es.wikipedia.org/wiki/Recursión_(ciencias_de_computación)"><em>funciones recursivas</em></a>.
+// Aquí tenemos un ejemplo clásico para calcular un factorial.
+
+package main
+
+import "fmt"
+
+// Esta función `fact` se llama a si misma hasta que llega a la
+// base de `fact(0)`.
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(fact(7))
+}
